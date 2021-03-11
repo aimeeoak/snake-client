c8e9dd1bbcc794fc19a4897ded9ab63b32dbc1fd
Aimee <aimeegarriock@gmail.com>
2021-03-11 04:09:34 +0000

    user interface setup

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    return stdin;
+  };
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const input = setupInput();
+input.on("data", dataTwoELectricBoogaloo => {
+  handleUserInput(dataTwoELectricBoogaloo);
+});
